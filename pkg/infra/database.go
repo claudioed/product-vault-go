86036cacaec7767a7f4d0720abd25c23c0cee090
claudioed <claudioed.oliveira@gmail.com>
2020-10-21 08:14:49 -0300

    load config from file

--- pkg/infra/database.go
+++ pkg/infra/database.go
@@ -2,18 +2,47 @@ package infra
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	logger "github.com/sirupsen/logrus"
 	"os"
 )
 
+type DbCred struct {
+	Username string `json:"DB_USER"`
+	Password string `json:"DB_PASS"`
+}
+
 func NewDatabaseConnection() *sql.DB {
 	var name = os.Getenv("DB_NAME")
-	var user = os.Getenv("DB_USER")
-	var pass = os.Getenv("DB_PASS")
 	var host = os.Getenv("DB_HOST")
-	dbConnectionString := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",user,pass,host,name)
-	logger.Info("Connection data %s",dbConnectionString)
+
+	f, err := os.Open("vault/secrets/product-go-vault-sidecar")
+	if err != nil {
+		logger.Error("error to load db cred %v", err.Error())
+		panic(err)
+	}
+
+	var cfg DbCred
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		logger.Errorf("fail to parse db cred %v", err.Error())
+		panic(err)
+	}
+
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		logger.Errorf("fail to write db cred %v", err.Error())
+		panic(err)
+	}
+
+	logger.Info("JSON")
+	logger.Info(string(data))
+
+	logger.Infof("User %v", cfg.Username)
+	dbConnectionString := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", cfg.Username, cfg.Password, host, name)
+	logger.Infof("Connection data %s", dbConnectionString)
 	db, err := sql.Open("postgres", dbConnectionString)
 	if err != nil {
 		logger.Error(err)
